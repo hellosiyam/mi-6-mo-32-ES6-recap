de7200ef40a718feecd3d8e7cce07dc5aee181fe
hellosiyam <hellosiyambd@gmail.com>
2025-01-24 18:42:17 +0600

    typeof-data.js

---
+++ typeof-data.js
@@ -0,0 +1,10 @@
+//Data type:
+
+//This is Premitive type Data;
+//1-String:
+//2-Number:
+//3-Bollin:
+
+//This is non-premitive type data;
+//4-Array:
+//5-Object:
---
+++ undeffine.js
@@ -0,0 +1,13 @@
+const students = ['siyam', 'rohul', 'asif', 'abir'];
+// students.splice(1);
+const student = students.slice(-2);
+console.log(student);
+
+const data = {
+    name: 'siyam',
+    age: 24,
+    live: 'bangladesh'
+}
+
+delete data.live;
+console.log(data);
