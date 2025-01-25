4096551eec68e905240f63c5e0a1509b73c4621e
hellosiyam <hellosiyambd@gmail.com>
2025-01-25 22:16:42 +0600

    typeof-data.js

---
+++ scope.js
@@ -0,0 +1,20 @@
+for (let i = 0; i < 10; i++) {
+    // console.log(i); 
+}
+// console.log('outSide:',i);
+
+
+print5()
+function print5(){
+    // console.log(2);
+    
+}
+
+
+
+function name() {
+    console.log(10);
+    
+}
+const add = name();
+console.log(add);
